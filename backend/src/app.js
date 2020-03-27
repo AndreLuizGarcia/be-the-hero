46800d80ac80468a83e5ea09bedba0c389c679b3
Andre Garcia <andre.luiz@gec.inatel.br>
2020-03-27 12:50:34 -0300

    Updated to build tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { errors } =require('celebrate')
 const routes = require('./routes')
 const cors = require('cors')
 
@@ -6,7 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
 app.use(routes)
+app.use(errors())
 
-app.listen(3333)
+module.exports = app
